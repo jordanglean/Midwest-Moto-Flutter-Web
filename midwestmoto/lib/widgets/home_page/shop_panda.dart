6f263c02960c17f83e20d84c6d7ff362deec096b
Jordan <jordanglean@outlook.com>
2021-02-04 17:31:29 +0000

    Added Shop links

--- midwestmoto/lib/widgets/home_page/shop_panda.dart
+++ midwestmoto/lib/widgets/home_page/shop_panda.dart
@@ -1,21 +1,35 @@
 import 'package:flutter/material.dart';
+// Widget Imports
+import 'package:midwestmoto/widgets/home_page/shop_panda_card.dart';
 
 // Shop Parts and Apparel
 class ShopPartAndApparel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 1600,
-      height: 1000,
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      height: 900,
       child: Column(
         children: <Widget>[
           Container(
+            margin: EdgeInsets.only(left: 20),
             width: double.infinity,
             padding: EdgeInsets.all(10),
             child: Text(
               'Shop Parts & Apparel',
               style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold),
             ),
+          ),
+          Padding(padding: EdgeInsets.all(10)),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ShopHomeCards(),
+              ShopHomeCards(),
+              ShopHomeCards(),
+            ],
           ),
         ],
       ),
---
+++ midwestmoto/lib/widgets/home_page/shop_panda_card.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+// Shop and Apparel Card=
+class ShopHomeCards extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black38,
+            spreadRadius: 3,
+            blurRadius: 10,
+            //offset: Offset(0, 2),
+          )
+        ],
+      ),
+      width: 500,
+      height: 700,
+      child: Column(
+        children: <Widget>[
+          Image.network(
+              'https://www.harley-davidson.com/content/dam/h-d/images/promo-images/2021/1x1/mens-apparel-1x1.jpg?impolicy=myresize&rw=650'),
+          Container(
+            margin: EdgeInsets.only(top: 5),
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: <Widget>[
+                Text(
+                  "MEN'S GEAR & APPAREL",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  "The latest styles in men's riding jackets, shirts, helmets, boots for winter.",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: Text("Shop Men's"),
+          )
+        ],
+      ),
+    );
+  }
+}
